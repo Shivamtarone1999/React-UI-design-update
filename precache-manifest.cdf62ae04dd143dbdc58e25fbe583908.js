953bdd25ebd0114f9e06a895b59b951cb58818e4
Shivam Tarone <shivamtarone07@gmail.com>
2020-10-02 23:24:13 +0530

    Updates

---
+++ precache-manifest.cdf62ae04dd143dbdc58e25fbe583908.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff691cb92766d926144febbb93ac7c0",
+    "url": "/React-UI-design-update/index.html"
+  },
+  {
+    "revision": "fdb18723eef74dcadcd0",
+    "url": "/React-UI-design-update/static/css/2.714b1602.chunk.css"
+  },
+  {
+    "revision": "6623d0f5c2cf64b8c9d8",
+    "url": "/React-UI-design-update/static/css/main.186319d3.chunk.css"
+  },
+  {
+    "revision": "fdb18723eef74dcadcd0",
+    "url": "/React-UI-design-update/static/js/2.d7ee6a22.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-UI-design-update/static/js/2.d7ee6a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6623d0f5c2cf64b8c9d8",
+    "url": "/React-UI-design-update/static/js/main.269a0b98.chunk.js"
+  },
+  {
+    "revision": "0a0d1148f557448b7261",
+    "url": "/React-UI-design-update/static/js/runtime-main.01d7c430.js"
+  },
+  {
+    "revision": "c0a71d5ee5f1586c832201dc1c773599",
+    "url": "/React-UI-design-update/static/media/1.c0a71d5e.svg"
+  },
+  {
+    "revision": "9b2e219a43fea0e79926ae4b2a7c6f61",
+    "url": "/React-UI-design-update/static/media/active__users.9b2e219a.svg"
+  },
+  {
+    "revision": "c1b2757061ca23126361317ad99c490a",
+    "url": "/React-UI-design-update/static/media/categories.c1b27570.svg"
+  },
+  {
+    "revision": "0ec28acf205b307321676c2e8a320b0e",
+    "url": "/React-UI-design-update/static/media/number__stories.0ec28acf.svg"
+  },
+  {
+    "revision": "8ec6241d6772ca32a6793512766634b3",
+    "url": "/React-UI-design-update/static/media/podcasts.8ec6241d.svg"
+  },
+  {
+    "revision": "b29a97988cf1d15cba617bb1412d97d9",
+    "url": "/React-UI-design-update/static/media/publications.b29a9798.svg"
+  },
+  {
+    "revision": "4c1eeb0e500732b9c64fad637c7b5996",
+    "url": "/React-UI-design-update/static/media/total__playing.4c1eeb0e.svg"
+  }
+]);
